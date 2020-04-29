35150fe06c31a978296f080b207eb31889c8d5fa
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-04-28 23:37:10 -0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,12 +4,47 @@ if(isset($_GET['checkInternet'])){
   $Result = pingIP('8.8.8.8');
   die(json_encode($Result));
 }
+if(isset($_GET['checkMainPower'])){
+  $Result = checkMainPower();
+  die(json_encode($Result));
+}
+if(isset($_GET['checkBackupPower'])){
+  $Result = checkBackupPower();
+  die(json_encode($Result));
+}
+if(isset($_GET['checkEnginePower'])){
+  $Result = checkEnginePower();
+  die(json_encode($Result));
+}
+if(isset($_GET['checkShorePower'])){
+  $Result = checkShorePower();
+  die(json_encode($Result));
+}
 
-function pingIP($ip) {
+
+function pingIP($ip){
   $pingresult = exec("/bin/ping -n -w 3 $ip", $outcome, $status);
-  if (0 == $status) {
+  if(0 == $status){
     return true;
-  } else {
+  }else{
     return false;
   }
+}
+function checkMainPower(){
+  return array(
+    'State'   => 'Online',
+    'Percent' => '75'
+  );
+}
+function checkBackupPower(){
+  return array(
+    'State'   => 'Online',
+    'Percent' => '100'
+  );
+}
+function checkEnginePower(){
+  return true;
+}
+function checkShorePower(){
+  return false;
 }
