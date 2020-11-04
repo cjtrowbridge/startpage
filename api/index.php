7bd6c7ca0bec44aa335bac0542cfcdc3cff36080
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-11-04 15:32:21 -0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -112,9 +112,36 @@ function getSurveillanceEvents(){
   $Ret = array();
   foreach($Events as $Time => $Event){
     $Ret[] = array(
+      'Ago'  => ago($Time),
       'Time' => date('Y-m-d H:i:s',$Time),
       'Link' => $Event
     );
   }
   return $Ret;
+}
+
+function ago($time){
+  /*
+    Ago accepts any date or time and returns a string explaining how long ago that was.
+    For example, "6 days ago" or "8 seconds ago"
+  */
+  if(intval($time)==0){
+    $time=strtotime($time);
+  }
+  if(($time==0)||(empty($time))){
+    return 'Never';
+  }
+  $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+  $lengths = array("60","60","24","7","4.35","12","10");
+  $now = time();
+  $difference     = $now - $time;
+  $tense         = "ago";
+  for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+    $difference /= $lengths[$j];
+  }
+  $difference = round($difference);
+  if($difference != 1) {
+    $periods[$j].= "s";
+  }
+  return "$difference $periods[$j] ago";
 }
